93e39ba43c37e61ad167c28178caae49cd972be0
ahuva12 <ahuvaeshit@gmail.com>
2024-11-06 09:36:56 +0200

    ss

--- src/app/page01/page.tsx
+++ src/app/page01/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { useEffect, useState } from "react";
-import {getAllCars, postCar, deleteCar, updateCar} from '@/services/crud';
+import { getAllCars, postCar, deleteCar, updateCar } from '@/services/crud';
 import { CarDocument } from '@/app/api/cars/route'
 
 const Home = () => {
-    const [cars, setCars] = useState<CarDocument[]>([]); 
+    const [cars, setCars] = useState<CarDocument[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const data = await getAllCars()
+            const data = await getAllCars();
             setCars(data);
             console.log(data);
-        }
+        };
         fetchData();
     }, []);
 
@@ -20,38 +20,44 @@ const Home = () => {
             model: "me3333",
             color: "white",
             price: 1453
-        }
+        };
         const res = await postCar(newCar);
         console.log("Car added successfully:", res);
-        setCars((prevCars) => [...prevCars, newCar]); 
-    }
+        setCars((prevCars) => [...prevCars, newCar]);
+    };
 
     const handleDeleteCar = async () => {
-        const cardId = '6729fb2bc547d6f4af94b261';
-        const res = await deleteCar(cardId);
+        const carId = '6729fb2bc547d6f4af94b261';
+        const res = await deleteCar(carId);
         console.log("Car deleted successfully:", res);
-        setCars(cars.filter(car => car._id !== cardId)); 
-    }
+        setCars(cars.filter(car => car._id !== carId));
+    };
 
     const handleUpdateCar = async () => {
         const updatedCar = {
             _id: "6729fb89c547d6f4af94b262",
             price: 2000
-        }
+        };
         const res = await updateCar(updatedCar);
         console.log("Car updated successfully:", res);
-        setCars((prevCars) => [...prevCars, res]); 
-    }
+        setCars((prevCars) => prevCars.map(car => car._id === updatedCar._id ? res : car));
+    };
 
     return (
         <div>
             <h1>Home</h1>
-            <p>cars</p>
-            <button onClick={() => handleAddCar()}>add car</button>
-            <button onClick={() => handleDeleteCar()}>delete car</button>
-            <button onClick={() => handleUpdateCar()}>update car</button>
+            <ul>
+                {cars.map((car) => (
+                    <li key={car._id}>
+                        Model: {car.model}, Color: {car.color}, Price: ${car.price}
+                    </li>
+                ))}
+            </ul>
+            <button onClick={handleAddCar}>Add Car</button>
+            <button onClick={handleDeleteCar}>Delete Car</button>
+            <button onClick={handleUpdateCar}>Update Car</button>
         </div>
-    )
+    );
 };
 
 export default Home;
